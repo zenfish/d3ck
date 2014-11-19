eefe653853724c67e2d2a30b111e6ad8d76da923
zenfish <zenfish@gmail.com>
2014-11-19 07:55:22 -0800

    starting again

--- linkage.sh
+++ linkage.sh
@@ -1,53 +1,82 @@
 #!/bin/bash -x
 
+#
+# a first cut at a setup script... try to setup 
+# various files, perms, and ownerships
+#
+
 me=$(who am i | awk '{print $1}')
 
-if [ $(pwd) = "/etc/d3ck" ] ; then
-    echo cannot run this script while in /etc/d3ck
-    exit 1
-fi
+dhome="/etc/d3ck"
 
-if [ -s /etc/d3ck ] ; then
-    echo removing /etc/d3ck symlink
+if [ $(pwd) != $dhome -a ! -s $dhome ] ; then
+    echo creating symlink in /etc/d3ck to current directory
     rm -f /etc/d3ck
+    sudo ln -s `pwd` /etc/d3ck
 fi
-if [ -d /etc/d3ck ] ; then
-    echo Looks like the D3CK is already installed, bailin out
-    exit 2
-fi
-# rm -rf /etc/d3ck
-# sudo mv /etc/d3ck /etc/d3ck.old
-sudo ln -s `pwd` /etc/d3ck
 
-sudo ls -l /etc/redis/redis.conf
-sudo ls -l /etc/init.d/d3ck
-sudo ls -l /usr/local/sbin/d3ckd
-sudo ls -l /etc/udev/rules.d/10-d3ck-key.rules
+echo creating additional symlinks...
+
+#
+# test if the last command actually worked
+#
+live_or_die () {
+   if [ $? != 0 ] ; then
+       echo "... failure..."
+       exit 2
+   fi
+}
+
+#
+# check to see if a file/link exists, move it aside, if not, symlink to the target
+#
+check_n_link() {
+    file="$1"
+    target="$2"
+
+    if [ -f $file -o -s $file ] ; then
+        # give me a break, lol
+        if [ -f $file.$$ -o -s $file.$$ ]; then
+            echo things are weird... $file.$$ exists... bailing out for safety\!
+            exit 3
+        fi
+
+        echo moving $file to $file.$$ so we can link to d3ck\'s version
+        mv $file $file.$$
+
+    fi
+
+    echo "creating symlink from $file -> $target"
+    ln -s $target $file
+
+    live_or_die
+
+}
 
 # redis has been a bit squirrely with naming... not sure what it is right now ;(
-if [ -f /etc/redis/redis.conf ] ; then
-    sudo mv /etc/redis/redis.conf /etc/redis/redis.conf.old
-fi
+check_n_link /etc/redis/redis.conf /etc/d3ck/redis/redis.conf
+check_n_link /etc/init.d/d3ck /etc/d3ck/init.d.d3ck
+check_n_link /usr/local/sbin/d3ckd /etc/d3ck/sbin.d3ckd
 
-sudo rm -f /etc/init.d/d3ck
-sudo rm -f /usr/local/sbin/d3ckd
-sudo rm -f /etc/udev/rules.d/10-d3ck-key.rules
+# check_n_link(/etc/udev/rules.d/10-d3ck-key.rules) tbd
 
-sudo ln -s `pwd` /etc/d3ck
 sudo mkdir /etc/d3ck/tmp
 sudo mkdir /etc/d3ck/uploads
 sudo mkdir /etc/d3ck/logs
 sudo mkdir /etc/d3ck/redis
 sudo mkdir /etc/d3ck/d3cks
+
 sudo chown redis.redis /etc/d3ck/redis
 
 touch /etc/d3ck/logs/client_vpn.log
 touch /etc/d3ck/logs/server_vpn.log
+
 chmod 777 /etc/d3ck/logs/client_vpn.log
 chmod 777 /etc/d3ck/logs/server_vpn.log
 
 sudo chown -R $me /etc/d3ck
 sudo chown -R redis /etc/d3ck/redis
+
 sudo chmod 755 /etc/d3ck
 sudo chmod 777 /etc/d3ck/tmp
 sudo chmod 777 /etc/d3ck/logs
@@ -61,6 +90,5 @@ sudo ln -s  /etc/d3ck/10-d3ck-key.rules /etc/udev/rules.d/10-d3ck-key.rules
 
 # in addition, some don't understand certain conf directives... here's
 # an alternate configuration file to use if, when redis starts, it dies
-
 sudo /etc/init.d/*redis* start
 
