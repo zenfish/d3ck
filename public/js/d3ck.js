4997bf3a4fab660aed80576d7d054ae4c2cdb62d
zenfish <zenfish@gmail.com>
2015-02-17 21:33:22 -0800

    cleanup

--- public/js/d3ck.js
+++ public/js/d3ck.js
@@ -2075,7 +2075,7 @@ function ask_user_4_response(data) {
             post_data.ip_addr     = req.from_ip
             post_data.did         = my_d3ck.D3CK_ID
             post_data.d3ck_action = "CREATE"
-            post_data.secret      = req.secret
+            post_data.secret      = req.secret.secret
 
             post_data             = JSON.stringify(post_data)
 
