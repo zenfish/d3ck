ceeccd7ee14f0fd1c05554ca3c15c30e6183bc54
zenfish <zenfish@gmail.com>
2015-03-03 21:00:57 -0800

    ....

--- main.js
+++ main.js
@@ -3039,17 +3039,12 @@ function serviceRequest(req, res, next) {
         //
         // var options = load_up_cc_cert(d3ckid)
 
-        options = {
-            ip_addr   : ip_addr,
-            d3ckid    : d3ckid,
+        options.form = req.body
+
+        options.d3ckid = {
             from_d3ck : bwana_d3ck.D3CK_ID,
             from      : bwana_d3ck.owner.name,
             url       : url,
-            owner     : owner,
-            service   : service,
-            secret    : secret,
-            knock     : true,
-            did       : d3ckid
         }
 
         log.info(options)
