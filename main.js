d478fabe4ab3b38bc96b637b3ccbb9d4ad594627
zenfish <zenfish@gmail.com>
2015-04-03 16:29:01 -0700

    new begins

--- main.js
+++ main.js
@@ -5316,6 +5316,8 @@ io_sig.set('log level', 1);
 
 cat_sock = io_sig.sockets
 
+/* 
+
 io_sig.sockets.on('connection', function (ss_client) {
 
     log.info("CONNEEEEECTION.....!")
@@ -5447,6 +5449,156 @@ io_sig.sockets.on('connection', function (ss_client) {
     // ss_client.emit('turnservers', credentials);
 
 });
+
+*/
+
+
+function describeRoom(name) {
+    var clients = io.sockets.clients(name);
+    var result = {
+        clients: {}
+    };
+    clients.forEach(function (client) {
+        result.clients[client.id] = client.resources;
+    });
+    return result;
+}
+
+function clientsInRoom(name) {
+    return io.sockets.clients(name).length;
+}
+
+function safeCb(cb) {
+    if (typeof cb === 'function') {
+        return cb;
+    } else {
+        return function () {};
+    }
+}
+
+io_sig.sockets.on('connection', function (ss_client) {
+
+
+    log.info("CONNEEEEECTION.....!")
+    // log.info(ss_client)
+
+    // a friendly cat fact
+    var cool_cat_fact = random_cat_fact(cat_facts)
+
+    cat_power({type: 'cat fax', 'line': cool_cat_fact})
+
+
+
+
+    ss_client.resources = {
+        screen: false,
+        video: true,
+        audio: false
+    };
+
+    // pass a message to another id
+    ss_client.on('message', function (details) {
+        if (!details) return;
+
+        var otherClient = io.sockets.sockets[details.to];
+        if (!otherClient) return;
+
+        details.from = ss_client.id;
+        otherClient.emit('message', details);
+    });
+
+    ss_client.on('shareScreen', function () {
+        ss_client.resources.screen = true;
+    });
+
+    ss_client.on('unshareScreen', function (type) {
+        ss_client.resources.screen = false;
+        removeFeed('screen');
+    });
+
+    ss_client.on('join', join);
+
+    function removeFeed(type) {
+        if (ss_client.room) {
+            io.sockets.in(ss_client.room).emit('remove', {
+                id: ss_client.id,
+                type: type
+            });
+            if (!type) {
+                ss_client.leave(ss_client.room);
+                ss_client.room = undefined;
+            }
+        }
+    }
+
+    function join(name, cb) {
+        // sanity check
+        if (typeof name !== 'string') return;
+        // check if maximum number of clients reached
+        if (config.rooms && config.rooms.maxClients > 0 && 
+          clientsInRoom(name) >= config.rooms.maxClients) {
+            safeCb(cb)('full');
+            return;
+        }
+        // leave any existing rooms
+        removeFeed();
+        safeCb(cb)(null, describeRoom(name));
+        ss_client.join(name);
+        ss_client.room = name;
+    }
+
+    // we don't want to pass "leave" directly because the
+    // event type string of "socket end" gets passed too.
+    ss_client.on('disconnect', function () {
+        removeFeed();
+    });
+    ss_client.on('leave', function () {
+        removeFeed();
+    });
+
+    ss_client.on('create', function (name, cb) {
+        if (arguments.length == 2) {
+            cb = (typeof cb == 'function') ? cb : function () {};
+            name = name || uuid();
+        } else {
+            cb = name;
+            name = uuid();
+        }
+        // check if exists
+        if (io.sockets.clients(name).length) {
+            safeCb(cb)('taken');
+        } else {
+            join(name);
+            safeCb(cb)(null, name);
+        }
+    });
+
+    // tell client about stun and turn servers and generate nonces
+//  ss_client.emit('stunservers', config.stunservers || []);
+
+    // create shared secret nonces for TURN authentication
+    // the process is described in draft-uberti-behave-turn-rest
+//  var credentials = [];
+//  config.turnservers.forEach(function (server) {
+//      var hmac = crypto.createHmac('sha1', server.secret);
+//      // default to 86400 seconds timeout unless specified
+//      var username = Math.floor(new Date().getTime() / 1000) + (server.expiry || 86400) + "";
+//      hmac.update(username);
+//      credentials.push({
+//          username: username,
+//          credential: hmac.digest('base64'),
+//          url: server.url
+//      });
+//  });
+//  ss_client.emit('turnservers', credentials);
+});
+
+
+
+
+
+
+
 
 
 // http://stackoverflow.com/questions/5223/length-of-javascript-object-ie-associative-array
