0596d58fcfbb1ef19b7804854a5236e11c6c8d41
zenfish <zenfish@gmail.com>
2015-04-12 21:16:06 -0700

    upgraded stuff... oh, the pain

--- public/js/R.js
+++ public/js/R.js
@@ -4968,8 +4968,9 @@ function WebRTC(opts) {
             // makes the entire PC config overridable
             peerConnectionConfig: {
                 // iceServers: [{"url": "stun:stun.l.google.com:19302"}]
+                // iceServers: [{"url": "stun:" + ICE_ICE_BABY}]
                 // iceServers: []
-                iceServers: [{"url": "stun:" + ICE_ICE_BABY}]
+                iceServers: [{"url": "stun:stun.services.mozilla.com"}]
             },
             peerConnectionConstraints: {
                 optional: [
