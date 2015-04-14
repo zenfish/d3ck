c51696e0a4fa2a0c20670096c2db16bb1f6fffeb
zenfish <zenfish@gmail.com>
2015-04-13 18:47:09 -0700

    upgraded stuff... oh, the pain

--- public/js/main_d3ck.js
+++ public/js/main_d3ck.js
@@ -13,6 +13,7 @@ ICE_PORT         = 3478
 
 SIGNALING_SERVER = 'https://' + window.location.hostname + ':' + D3CK_SIG_PORT
 ICE_HOST         = window.location.hostname
+ICE_P2P_PORT     = 5551
 ICE_ICE_BABY     = ICE_HOST + ':' + ICE_PORT
 
 // poll until we get something, then stop polling
