f13b5bea486a72866dc8429517a8c98c9c133a7f
zenfish <zenfish@gmail.com>
2015-04-28 10:20:59 -0700

    latest

--- main.js
+++ main.js
@@ -5538,7 +5538,27 @@ io_sig = require('socket.io').listen(d3cky)
 
 // ... just... fuck it all
 // io_sig.set('origin', '*');
-io_sig.use(cors())
+
+io_sig.on('request', function(req, res) {
+    if(req.method === 'OPTIONS' && req.url.indexOf('/socket.io') === 0) {
+      var headers = {};
+      if (req.headers.origin) {
+        headers['Access-Control-Allow-Credentials'] = 'true';
+        headers['Access-Control-Allow-Origin'] = req.headers.origin;
+      } else {
+        headers['Access-Control-Allow-Origin'] = '*';
+      }
+
+      headers['Access-Control-Allow-Methods'] = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+      headers['Access-Control-Allow-Headers'] = 'origin, content-type, accept';
+      res.writeHead(200, headers);
+      res.end();
+    } else {
+      listeners.forEach(function(fn) {
+        fn.call(srv, req, res);
+      });
+    }
+});
 
 
 
